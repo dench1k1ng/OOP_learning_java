030a26f55041c6080e82339170bd50ad38d60dd0
Shindoril <denisshin2000@gmail.com>
2024-12-23 18:55:43 +0500

    keep working on assignment 2

---
+++ src/Assignment_2_1/Solution_9.java
@@ -0,0 +1,12 @@
+package Assignment_2_1;
+
+import java.sql.SQLOutput;
+
+public class Solution_9 {
+    public static void main(String[] args) {
+        int[][] matrix = new int[10][];
+        for (int i = 0; i < 10; i++) {
+            System.out.println(matrix[i]);;
+        }
+    }
+}
