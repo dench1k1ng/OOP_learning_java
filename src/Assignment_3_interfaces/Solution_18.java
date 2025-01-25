06d6992ed92d64ac8a4660b45d5f4fc1fa8f253e
Shindoril <denisshin2000@gmail.com>
2025-01-25 11:11:02 +0500

    finish assignment 3

--- src/Assignment_3_interfaces/Solution_18.java
+++ src/Assignment_3_interfaces/Solution_18.java
@@ -1,49 +1,61 @@
 package Assignment_3_interfaces;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution_18 {
     public static void main(String[] args) {
-        List<Person> plot = new ArrayList<Person>();
-        plot.add(new Person("Repka", "Repka"));
+        List<Person> plot = new ArrayList<>();
+        plot.add(new Person("Repka", "Repku"));
         plot.add(new Person("Dedka", "Dedku"));
-        plot.add(new Person("Babka", "Babka"));
+        plot.add(new Person("Babka", "Babku"));
         plot.add(new Person("Granddaughter", "Granddaughter"));
+
         RepkaStory.tell(plot);
     }
+}
 
-    public class Person {
-        private String name;
-        private String namePadezh;
-        public Person(String name, String namePadezh) {
-            this.name = name;
-            this.namePadezh = namePadezh;
-        }
-        public String getName() {
-            return name;
-        }
-        public void setName(String name) {
-            this.name = name;
-        }
-        public String getNamePadezh() {
-            return namePadezh;
-        }
-        public void setNamePadezh(String namePadezh) {
-            this.namePadezh = namePadezh;
-        }
+class Person implements RepkaItem {
+    private String name;
+    private String namePadezh;
+
+    public Person(String name, String namePadezh) {
+        this.name = name;
+        this.namePadezh = namePadezh;
     }
 
-    public interface RepkaItem {
-        Public String getNamePadezh();
+    public String getName() {
+        return name;
     }
 
-    public class RepkaStory {
-        static void tell(List<Person> items) {
-            Person first;
-            Person two;
-            for (int i = items.size() - 1; i > 0; i--) {
-                first = items.get(i - 1);
-                second = items.get(i);
-                first.pull(second);
-            }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getNamePadezh() {
+        return namePadezh;
+    }
+
+    public void setNamePadezh(String namePadezh) {
+        this.namePadezh = namePadezh;
+    }
+
+    public void pull(Person person) {
+        System.out.println(this.name + " for " + person.getNamePadezh());
+    }
+}
+
+interface RepkaItem {
+    String getNamePadezh();
+}
+
+class RepkaStory {
+    static void tell(List<Person> items) {
+        for (int i = items.size() - 1; i > 0; i--) {
+            Person first = items.get(i - 1);
+            Person second = items.get(i);
+            first.pull(second);
         }
     }
 }
